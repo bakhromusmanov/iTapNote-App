e8aa51c1df7cb397d55fe4500872e9dd44565706
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-07-01 22:00:50 +0500

    Initial Commit

---
+++ iTapNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTapNote
+//
+//  Created by MacBook Air on 01/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
